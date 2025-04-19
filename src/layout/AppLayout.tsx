91d8f6b7187f2a06c7516a84a867955dca09557c
Mykhailo <ivchenkomykhailo@gmail.com>
2025-04-19 20:00:53 +0300

    Add a provider for react toastify, scroll to top component and common app layout

---
+++ src/layout/AppLayout.tsx
@@ -0,0 +1,18 @@
+import ScrollToTop from '@/layout/components/ScrollToTop';
+import ToastProvider from '@/layout/components/ToastProvider';
+
+import { ErrorBoundary } from 'react-error-boundary';
+import { Outlet } from 'react-router';
+import FallBackRender from '@/layout/ErrorBoundary/FallBackRender';
+
+const AppLayout = () => {
+  return (
+    <ErrorBoundary fallbackRender={FallBackRender}>
+      <Outlet />
+      <ScrollToTop />
+      <ToastProvider />
+    </ErrorBoundary>
+  );
+};
+
+export default AppLayout;
