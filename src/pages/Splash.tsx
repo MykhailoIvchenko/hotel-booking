bad4ece86f4740e1d9c97be61243fd907d992633
Mykhailo <ivchenkomykhailo@gmail.com>
2025-04-19 22:20:55 +0300

    Add backround for the splash page

--- src/pages/Splash.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Splash: React.FC = () => {
-  return <div></div>;
-};
-
-export default Splash;
---
+++ src/pages/Splash/Splash.tsx
@@ -0,0 +1,7 @@
+import styles from './splash.module.css';
+
+const Splash: React.FC = () => {
+  return <div className={styles.container}></div>;
+};
+
+export default Splash;
