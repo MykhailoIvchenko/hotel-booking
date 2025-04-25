20ef78a0bb4a0e78996811d4ae40a982c89b73a6
Mykhailo <ivchenkomykhailo@gmail.com>
2025-04-25 22:36:33 +0300

    Implement an Input component

---
+++ src/components/input/Input.tsx
@@ -0,0 +1,58 @@
+import clsx from 'clsx';
+import { ReactNode, InputHTMLAttributes } from 'react';
+import styles from './input.module.css';
+
+interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  inputIcon?: ReactNode;
+  iconPosition: 'left' | 'right';
+  error?: string;
+  wrapperClassName?: string;
+  caliber?: 'xs' | 'md' | 'lg';
+  ref?: React.Ref<HTMLInputElement>;
+}
+
+const Input: React.FC<IInputProps> = ({
+  label,
+  error,
+  wrapperClassName,
+  inputIcon,
+  iconPosition = 'left',
+  className,
+  caliber = 'md',
+  ref,
+  ...props
+}) => {
+  return (
+    <div
+      className={clsx(
+        styles.wrapper,
+        wrapperClassName,
+        error && styles.inputError
+      )}
+    >
+      {label && <label className={styles.label}>{label}</label>}
+
+      <div
+        className={clsx(
+          styles.inputWrapper,
+          iconPosition &&
+            iconPosition === 'right' &&
+            styles['inputWrapper--reversed']
+        )}
+      >
+        {inputIcon}
+
+        <input
+          ref={ref}
+          className={clsx(styles.input, styles[`input--${caliber}`], className)}
+          {...props}
+        />
+      </div>
+
+      {error && <span className={styles.error}>{error}</span>}
+    </div>
+  );
+};
+
+export default Input;
