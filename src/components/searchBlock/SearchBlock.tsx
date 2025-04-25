dbb98b9d89a269ec334f21ad5cd32f091477c8c0
Mykhailo <ivchenkomykhailo@gmail.com>
2025-04-25 23:55:50 +0300

    Add a basic button component, use it in the search block

--- src/components/searchBlock/SearchBlock.tsx
+++ src/components/searchBlock/SearchBlock.tsx
@@ -1,8 +1,9 @@
 import clsx from 'clsx';
-import styles from './searchBlock.module.css';
 import SearchIcon from '@/assets/icons/search.svg?react';
 import Input from '@/components/input/Input';
 import FilterDropdown from './filterDropdown/FilterDropdown';
+import Button from '@/components/button/Button';
+import styles from './searchBlock.module.css';
 
 interface ISearchBlockProps {
   addClasses?: string;
@@ -12,13 +13,20 @@ const SearchBlock: React.FC<ISearchBlockProps> = ({ addClasses }) => {
   return (
     <div className={clsx(styles.container, addClasses)}>
       <Input
-        inputIcon={<SearchIcon style={{ color: '#888E91' }} />}
+        inputIcon={
+          <SearchIcon style={{ color: '#888E91', width: 24, height: 24 }} />
+        }
         iconPosition={'left'}
         placeholder='Search by Hotel name'
         wrapperClassName={styles.searchInput}
       />
 
       <FilterDropdown />
+
+      <Button variant='primary' addClasses={styles.button}>
+        Search
+        <SearchIcon className={styles.buttonIcon} />
+      </Button>
     </div>
   );
 };
