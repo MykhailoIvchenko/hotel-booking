ef6a430450c557bbed12ecca298e673d66121459
Mykhailo <ivchenkomykhailo@gmail.com>
2025-05-04 17:21:37 +0300

    Implement the contact button

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router';
 import Header from './header/Header';
 import Footer from './footer/Footer';
+import ContactButton from '@/components/contactButton/ContactButton';
 
 const DefaultLayout = () => {
   return (
@@ -10,6 +11,8 @@ const DefaultLayout = () => {
       <main>
         <Outlet />
       </main>
+
+      <ContactButton />
 
       <Footer />
     </div>
