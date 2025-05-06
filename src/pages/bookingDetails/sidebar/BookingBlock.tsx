404ee67cda461c65be8c3e4aaf57478db2596fb7
Mykhailo <ivchenkomykhailo@gmail.com>
2025-05-06 21:55:40 +0300

    Add pricing info, title for sidebar, add an accordion component

--- src/pages/bookingDetails/sidebar/BookingBlock.tsx
+++ src/pages/bookingDetails/sidebar/BookingBlock.tsx
@@ -1,7 +1,29 @@
+import Button from '@/components/button/Button';
 import styles from './bookingBlock.module.css';
+import { Link } from 'react-router';
 
 const BookingBlock: React.FC = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.titleContainer}>
+        <h5 className={styles.title}>Booking</h5>
+      </div>
+
+      <div>
+        <div className={styles.priceTitle}>subtotal</div>
+
+        <p className={styles.price}>$148.25</p>
+      </div>
+
+      <Button type={'button'} variant={'primary'}>
+        Confirm Booking
+      </Button>
+
+      <Link to={'#'} className={styles.policyLink}>
+        Cancellation Policy
+      </Link>
+    </div>
+  );
 };
 
 export default BookingBlock;
