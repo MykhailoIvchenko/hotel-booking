b9e6cabe284183c63a4bb2cbb523a4befe7138f4
Mykhailo <ivchenkomykhailo@gmail.com>
2025-05-27 21:19:34 +0300

    Implement the basic server project part structure

---
+++ server/src/server.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.send('API is running...');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
