5ed26e008e51bc2185ebbaaed3e9dc3a28f2b876
Mykhailo <ivchenkomykhailo@gmail.com>
2025-05-28 20:30:48 +0300

    Add basic file for api errors

---
+++ server/src/exceptions/ApiError.ts
@@ -0,0 +1,29 @@
+type ErrorDetails = Record<string, string[]>;
+
+export class ApiError extends Error {
+  status: number;
+  errors: ErrorDetails;
+
+  constructor(status: number, message: string, errors: ErrorDetails = {}) {
+    super(message);
+    this.name = 'ApiError';
+    this.status = status;
+    this.errors = errors;
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, ApiError);
+    }
+  }
+
+  static BadRequest(message: string, errors: ErrorDetails = {}): ApiError {
+    return new ApiError(400, message, errors);
+  }
+
+  static Unauthorized(): ApiError {
+    return new ApiError(401, 'User is not authorized');
+  }
+
+  static NotFound(): ApiError {
+    return new ApiError(404, 'Not found');
+  }
+}
