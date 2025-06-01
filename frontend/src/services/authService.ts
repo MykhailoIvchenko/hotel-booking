3d233cfcb741559737a5380dff294a40a948d76a
Mykhailo <ivchenkomykhailo@gmail.com>
2025-06-01 10:31:00 +0300

    Implement auth service

---
+++ frontend/src/services/authService.ts
@@ -0,0 +1,21 @@
+import { BasicEndpoints, Endpoints } from '@/utils/enums';
+
+export const authService = {
+  async refresh() {
+    const response = await fetch(
+      `${import.meta.env.VITE_BASE_API_URL}${BasicEndpoints.Auth}${
+        Endpoints.Refresh
+      }`,
+      {
+        method: 'POST',
+        credentials: 'include',
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error('Refresh token failed');
+    }
+
+    return response.json();
+  },
+};
