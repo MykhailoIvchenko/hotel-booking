b39384743f10be1a69a507c98475dfd2e2484d9c
Mykhailo <ivchenkomykhailo@gmail.com>
2025-06-07 09:47:32 +0300

    Implement the notification controller

---
+++ server/src/controllers/notificationController.ts
@@ -0,0 +1,42 @@
+import { notificationService } from '../services/notificationService.js';
+import { Request, Response } from 'express';
+import { ApiError } from '../exceptions/ApiError.js';
+
+export const notificationController = {
+  async getAllForUser(req: Request, res: Response): Promise<void> {
+    const userId = req.user?.id;
+    if (!userId) {
+      throw ApiError.Unauthorized();
+    }
+    const notifications = await notificationService.getAllForUser(userId);
+    res.send(notifications);
+  },
+
+  async makeReadOne(req: Request, res: Response): Promise<void> {
+    const userId = req.user?.id;
+    const notificationId = req.params.id;
+
+    if (!userId || !notificationId) {
+      throw ApiError.BadRequest('Notification id is required');
+    }
+
+    const updatedNotification = await notificationService.makeReadOne(
+      userId,
+      notificationId
+    );
+    if (!updatedNotification) {
+      throw ApiError.NotFound();
+    }
+
+    res.send(updatedNotification);
+  },
+
+  async makeReadAll(req: Request, res: Response): Promise<void> {
+    const userId = req.user?.id;
+    if (!userId) {
+      throw ApiError.Unauthorized();
+    }
+    const updatedCount = await notificationService.makeReadAll(userId);
+    res.send({ updatedCount });
+  },
+};
